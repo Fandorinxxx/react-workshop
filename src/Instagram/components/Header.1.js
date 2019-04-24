5dcb3449289b301da290af0c8ac889b9482eeb8d
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-04-24 17:48:34 +0300

    → Checkpoint at: 20:55:33

---
+++ src/Instagram/components/Header.1.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div className = 'header'>
+            <div className = 'logo' />
+            <input
+                placeholder = 'Search'
+                type = 'text'
+            />
+            <div className = 'nav'>
+                <div className = 'explore' />
+                <div className = 'likes' />
+                <div className = 'settings' />
+            </div>
+        </div>
+    );
+};
