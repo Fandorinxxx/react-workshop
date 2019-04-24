00df968b0c7fff291df1d5b5452e21f4297ec5e0
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-04-24 17:48:34 +0300

    → Checkpoint at: 20:41:04

--- src/Instagram/index.js
+++ src/Instagram/index.js
@@ -15,6 +15,9 @@ export const Instagram = () => {
                         className = 'avatar'
                         src = { avatar }
                     />
+                    <div className = 'main'>
+                        <span className = 'nickname'>nasa</span>
+                    </div>
                 </div>
             </div>
         </>
