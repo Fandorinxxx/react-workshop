e11890a8c9bc2225beddd6f7acc4b8a0eaa448cc
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-04-24 17:48:34 +0300

    → Checkpoint at: 20:57:12

--- src/Instagram/index.js
+++ src/Instagram/index.js
@@ -1,55 +1,13 @@
 import React from 'react';
 import './styles/main.css';
 import { Header } from './components/Header';
-
-import avatar from './assets/images/nasa.jpg';
-import images from './assets/data/images.json';
+import { Content } from './components/Content';
 
 export const Instagram = () => {
-    const imagesJSX = images.map((image) => {
-        return (
-            <div
-                className = 'image'
-                key = { image.id }>
-                <img
-                    alt = 'instaphoto'
-                    src = { image.src }
-                />
-            </div>
-        );
-    });
-
     return (
         <>
             <Header />
-            <div className = 'content'>
-                <div className = 'profile'>
-                    <img
-                        alt = 'avatar'
-                        className = 'avatar'
-                        src = { avatar }
-                    />
-                    <div className = 'main'>
-                        <span className = 'nickname'>nasa</span>
-                        <div className = 'stats'>
-                            <div>
-                                <span>2,436&nbsp;</span>
-                                posts
-                            </div>
-                            <div>
-                                <span>34,9m&nbsp;</span>
-                                followers
-                            </div>
-                            <div>
-                                <span>70&nbsp;</span>
-                                following
-                            </div>
-                        </div>
-                        <span className = 'name'>NASA</span>
-                    </div>
-                </div>
-                <div className = 'images'>{imagesJSX}</div>
-            </div>
+            <Content />
         </>
     );
 };
