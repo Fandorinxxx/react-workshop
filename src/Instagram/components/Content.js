9085d08e166a76d1b182d073352a3e7aa66edbde
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-04-24 17:48:34 +0300

    → Checkpoint at: 20:56:04

--- src/Instagram/components/Content.js
+++ src/Instagram/components/Content.js
@@ -1,18 +1,34 @@
 import React from 'react';
 
-export const Header = () => {
+export const Content = () => {
     return (
-        <div className = 'header'>
-            <div className = 'logo' />
-            <input
-                placeholder = 'Search'
-                type = 'text'
-            />
-            <div className = 'nav'>
-                <div className = 'explore' />
-                <div className = 'likes' />
-                <div className = 'settings' />
+        <div className = 'content'>
+            <div className = 'profile'>
+                <img
+                    alt = 'avatar'
+                    className = 'avatar'
+                    src = { avatar }
+                />
+                <div className = 'main'>
+                    <span className = 'nickname'>nasa</span>
+                    <div className = 'stats'>
+                        <div>
+                            <span>2,436&nbsp;</span>
+                            posts
+                        </div>
+                        <div>
+                            <span>34,9m&nbsp;</span>
+                            followers
+                        </div>
+                        <div>
+                            <span>70&nbsp;</span>
+                            following
+                        </div>
+                    </div>
+                    <span className = 'name'>NASA</span>
+                </div>
             </div>
+            <div className = 'images'>{imagesJSX}</div>
         </div>
     );
 };
